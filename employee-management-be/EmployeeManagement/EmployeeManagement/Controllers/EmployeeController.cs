e5c4654f45cec1e5a3ce73e4f90097aebc6a3ab2
ACCESA\fabian.ardeiu <fabian.ardeiu@accesa.eu>
2022-05-24 16:10:45 +0300

    Add first controller

---
+++ employee-management-be/EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using EmployeeManagement.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> _employees = new List<Employee> 
+        {
+            new Employee
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "First name",
+                LastName = "Last name",
+                Age = 10,
+                DateOfBirth = DateTime.Now.AddYears(-10),
+                Company = "OWL",
+                IsDirector = false
+            },
+            new Employee
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "First name 2",
+                LastName = "Last name 2",
+                Age = 20,
+                DateOfBirth = DateTime.Now.AddYears(-20),
+                Company = "OWL 2",
+                IsDirector = true
+            }
+
+        };
+
+        public EmployeeController()
+        {
+        }
+
+        [HttpGet]
+        [Route("employees")]
+        public IActionResult GetEmployees()
+        {
+            return Ok(_employees);
+        }
+
+        [HttpGet]
+        public IActionResult GetEmployee([FromQuery]Guid employeeId)
+        {
+            var employee = _employees.FirstOrDefault(e => e.Id == employeeId);
+
+            if (employee == null)
+            {
+                return NotFound("Employee with given id could not be found");
+            }
+            else
+            {
+                return Ok(employee);
+            }
+        }
+    }
+}
---
+++ employee-management-be/EmployeeManagement/EmployeeManagement/Domain/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeManagement.Domain
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Company { get; set; }
+        public bool IsDirector { get; set; }
+    }
+}
